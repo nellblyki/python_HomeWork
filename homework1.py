079ce1c0dd85ddc14a05599c910ab12e22a9e978
nellblyki <artemii.kharlamov7@gmail.com>
2025-01-28 11:26:01 +0300

    Create homework1.py

---
+++ homework1.py
@@ -0,0 +1,8 @@
+#Create a function that takes an integer as an argument and returns for even numbers or for odd numbers."Even""Odd"
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+    
